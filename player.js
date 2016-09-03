a65d478a5d6102889a243c97f0410df7096d5982
Dan Danciu <ddanciuster@gmail.com>
2016-09-03 10:14:37 +0300

    logs

--- player.js
+++ player.js
@@ -4,6 +4,12 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
+      
+      
+      
+    console.out(game_state.players);  
+      
+      
     bet(200);
   },
 
