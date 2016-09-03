f850ab0e8342749305991c45a620c9c69ff458e9
Dan Danciu <ddanciuster@gmail.com>
2016-09-03 10:28:25 +0300

    test infra

--- player.js
+++ player.js
@@ -9,11 +9,15 @@ module.exports = {
     if(game_state)
         console.log(JSON.stringify(game_state));  
       
+      for (var player in game_state.players)
+      
       
     bet(200);
   },
     
-    
+  hand : function(game_state) {
+      return undefined;
+  },    
 
   showdown: function(game_state) {
 
