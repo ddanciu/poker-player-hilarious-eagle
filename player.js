cd92fb9c178f81294232186146fafc7a40ee4d3f
Dan Danciu <ddanciuster@gmail.com>
2016-09-03 10:18:49 +0300

    logs

--- player.js
+++ player.js
@@ -7,7 +7,7 @@ module.exports = {
       
       
     if(game_state)
-        console.out(JSON.stringify(game_state.players));  
+        console.out(JSON.stringify(game_state));  
       
       
     bet(200);
