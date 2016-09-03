53be3ed07fdbeb06a5dba8ae1808bd1b4e966bf2
Silvia Rausanu <silvia.rausanu@gmail.com>
2016-09-03 11:53:42 +0300

    Revert " new poker solver"
    This reverts commit cb9913c11eb3235636c34bc9eceb4997a4d1cbca.

--- player.js
+++ player.js
@@ -1,24 +1,10 @@
-var _ = require('underscore'),
-    whatIs = require('poker-has');
-
-
-var extractHandFromGame = function(game_state){
-  return [{rank:'2', type:'D'},
-    {rank:'3', type:'D'}];
-};
 
 module.exports = {
 
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-
-    var handToBetOn = extractHandFromGame(game_state),
-        hand = whatIs(handToBetOn),
-        handRank = hand.strength;
-
-    console.log("handRank: " + handRank);
-
+    
     if(game_state)
         console.log(JSON.stringify(game_state));  
 
