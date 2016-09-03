d67a24d51e4b1714f2321f8725dc572553d0e1c1
Dan Danciu <ddanciuster@gmail.com>
2016-09-03 11:45:34 +0300

    Revert " add dependencies + function declaration"
    This reverts commit 59b6f5a37ab886e7410dd1125d3cc23b558fab63.

--- player.js
+++ player.js
@@ -1,20 +1,9 @@
-var _ = require('underscore'),
-    Hand = require('pokersolver').Hand;
-
-
-var extractHandFromGame = function(game_state){
-  return ['As', 'Ac'];
-};
 
 module.exports = {
 
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-
-    var handToBetOn = extractHandFromGame(game_state),
-        hand = Hand.solve(handToBetOn),
-        handRank = hand.rank;
     
     if(game_state)
         console.log(JSON.stringify(game_state));  
