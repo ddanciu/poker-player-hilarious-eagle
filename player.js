f28a1b1f03a008c1ca459386029d0e9c973818d3
Silvia Rausanu <silvia.rausanu@gmail.com>
2016-09-03 11:35:34 +0300

    add dependencies

--- player.js
+++ player.js
@@ -4,13 +4,10 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state, bet) {
-      
-      
+    
     if(game_state)
         console.log(JSON.stringify(game_state));  
 
-      
-      
     bet(200);
   },
     
